4da2186664eb36524222662164f166e48a133415
Vlad Krupen <air7360@gmail.com>
2024-01-03 20:02:56 +0300

    a class BankAccount and threads are created that perform banking operations

--- Homework_Thread/ViewController.swift
+++ Homework_Thread/ViewController.swift
@@ -7,11 +7,74 @@
 
 import UIKit
 
+class BankAccount {
+    
+    var accountBalance: Float = 0
+    
+    private let lock = NSLock()
+    
+    func deposit(ammount: Float) {
+        lock.lock()
+        accountBalance += ammount
+        print("Пополнение: \(ammount), текущий баланс: \(accountBalance)")
+        defer {
+            lock.unlock()
+        }
+    }
+    
+    func withdraw(ammount: Float) {
+        lock.lock()
+        if accountBalance >= ammount {
+            accountBalance -= ammount
+            print("Со счета снято: \(ammount), текущий баланс: \(accountBalance)")
+        } else {
+            print("Операция невозможна: недостаточно средств")
+        }
+        defer {
+            lock.unlock()
+        }
+        
+    }
+    
+    
+}
+
 class ViewController: UIViewController {
+    
+    let account = BankAccount()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let firstThread = Thread {
+            self.account.deposit(ammount: 100)
+//            print("Баланс: \(self.account.accountBalance)")
+        }
+        
+        let secondThread = Thread {
+            self.account.withdraw(ammount: 70)
+        }
+        
+        let thirdThread = Thread {
+            self.account.withdraw(ammount: 90)
+        }
+        
+        let fourthThread = Thread {
+            self.account.deposit(ammount: 300)
+        }
+        
+        let fifthThread = Thread {
+            self.account.deposit(ammount: 50)
+        }
+        
+        
+        firstThread.start()
+        secondThread.start()
+        thirdThread.start()
+        fourthThread.start()
+        fifthThread.start()
+        
+        
     }
 
 
